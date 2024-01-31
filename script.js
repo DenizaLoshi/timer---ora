7477e1746fed23ffe22cbff7fa67c83330776689
DenizaLoshi <deniza.loshi@web.de>
2024-01-31 16:48:09 +0100

    timer

---
+++ script.js
@@ -0,0 +1,12 @@
+let intervalId;
+function shfaqOren() {
+    const clock = new Date();
+    const ora = clock.toLocaleTimeString();
+    document.getElementById('ora').innerHTML = ora;
+}
+function startInterval() {
+    shfaqOren();
+    intervalId = setInterval(shfaqOren, 1000);
+}
+
+startInterval();
